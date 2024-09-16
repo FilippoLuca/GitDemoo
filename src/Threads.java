0bacf69eed21add415ad8b505c3f0ecb558c168a
hrenc <mironmirel9@yahoo.com>
2024-09-16 18:38:54 +0300

    first commit

---
+++ src/Threads.java
@@ -0,0 +1,15 @@
+//@FunctionalInterface
+//interface Runnable {
+//    void run();
+//}
+public class Threads {
+
+    public static void main(String[] args) {
+//        new Thread(() -> System.out.print()).start();
+//        System.out.println("World");
+
+        Runnable printInventory = () -> System.out.println("Printing zoo inventory");
+        Runnable printRecords = () -> { for}
+
+    }
+}
